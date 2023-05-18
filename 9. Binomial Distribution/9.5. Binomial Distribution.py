6261d9c7401e5fa03bf3af9c79f7faa7f3519497
Seng Keat <corodostudio@gmail.com>
2023-05-18 20:14:29 +0700

    Create 9.5. Binomial Distribution.py

---
+++ 9. Binomial Distribution/9.5. Binomial Distribution.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def binomial_distribution(n, p):
+  """
+  Calculates the probability of getting k successes in n trials,
+  where each trial has a probability of success of p.
+
+  Args:
+    n: The number of trials.
+    p: The probability of success in each trial.
+
+  Returns:
+    A list of probabilities, one for each possible number of successes.
+  """
+
+  probabilities = []
+  for k in range(n + 1):
+    probabilities.append(np.math.factorial(n) / (np.math.factorial(k) * np.math.factorial(n - k)) * p ** k * (1 - p) ** (n - k))
+
+  return probabilities
+
+This code can be used to calculate the probability of getting any number of successes in a binomial distribution. For example, to calculate the probability of getting exactly 5 successes in 10 trials, where the probability of success in each trial is 0.5, you would use the following code:
